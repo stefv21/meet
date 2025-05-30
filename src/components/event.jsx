905373f04b28fb1b308d40cda18aa2f8d2a00c05
stefv21 <161451930+stefv21@users.noreply.github.com>
2025-05-30 07:44:40 -0400

    Update event.jsx

--- src/components/event.jsx
+++ src/components/event.jsx
@@ -9,7 +9,7 @@ const Event = ({ event }) => {
       <p>{event.created}</p>
       <p>{event.location}</p>
       <button
-        className='details-button'
+        className="details-btn"
         onClick={() => setShowDetails(!showDetails)}
       >
         {showDetails ? 'Hide Details' : 'Show Details'}
