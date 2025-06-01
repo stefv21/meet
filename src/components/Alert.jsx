dc889ef26c8d38f8a80670361ff5fb4c09bd8a65
stefv21 <161451930+stefv21@users.noreply.github.com>
2025-06-01 09:00:01 -0400

    updated alert.jsx

---
+++ src/components/Alert.jsx
@@ -0,0 +1,34 @@
+import { Component } from 'react';
+
+
+class Alert extends Component {
+ constructor(props) {
+   super(props);
+   this.color = null;
+   this.bgColor = null;
+ }
+
+ getStyle = () => {
+    return {
+      color: this.color,
+      backgroundColor: this.bgColor,
+      borderWidth: "2px",
+      borderStyle: "solid",
+      fontWeight: "bolder",
+      borderRadius: "7px",
+      borderColor: this.color,
+      textAlign: "center",
+      fontSize: "12px",
+      margin: "10px 0",
+      padding: "10px"
+    };
+  }
+
+  render() {
+    return (
+      <div className="Alert">
+        <p style={this.getStyle()}>{this.props.text}</p>
+      </div>
+    );
+  }
+ }
