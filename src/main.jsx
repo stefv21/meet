ff2493125d669dc8dc88bf6a01fc776e26a34b87
stefv21 <161451930+stefv21@users.noreply.github.com>
2025-06-01 20:59:28 -0400

    updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import * as atatus from 'atatus-spa';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 atatus.config('ee138676f30c49858a9233a6cf6a3105').install();
 
 
@@ -12,6 +14,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
+
+serviceWorkerRegistration.register();
+
+
 
 
