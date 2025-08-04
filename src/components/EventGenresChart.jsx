b464990230cf335eedf49aa600ed5ccfd88a66f9
stefv21 <161451930+stefv21@users.noreply.github.com>
2025-08-04 12:27:45 -0400

    Fix pie chart circle being cut off by adding proper margins

--- src/components/EventGenresChart.jsx
+++ src/components/EventGenresChart.jsx
@@ -55,14 +55,14 @@ const EventGenresChart = ({ events }) => {
 
   return (
     <ResponsiveContainer width="99%" height={400}>
-      <PieChart>
+      <PieChart margin={{ top: 20, right: 80, bottom: 20, left: 80 }}>
         <Pie
           data={data}
           dataKey="value"
           fill="#8884d8"
           labelLine={false}
           label={renderCustomizedLabel} 
-          outerRadius={150} 
+          outerRadius={120} 
         />
       </PieChart>
     </ResponsiveContainer>
